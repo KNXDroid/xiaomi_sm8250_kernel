c7f1601b345c50d8db76ca6a3e9e640e8b9f6753
EmanuelCN <emanuelghub@gmail.com>
2025-09-04 21:55:54 +0300

    treewide: Squash backport bpf from 5.10
    Entire history is present in bpf branch
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1768,6 +1768,7 @@ static void vb_free(const void *addr, unsigned long size)
 }
 
 static void _vm_unmap_aliases(unsigned long start, unsigned long end, int flush)
+<<<<<<< HEAD
 {
 	LIST_HEAD(purge_list);
 	int cpu;
@@ -1817,6 +1818,31 @@ static void _vm_unmap_aliases(unsigned long start, unsigned long end, int flush)
 	if (!__purge_vmap_area_lazy(start, end) && flush)
 		flush_tlb_kernel_range(start, end);
 	mutex_unlock(&vmap_purge_lock);
+}
+
+/**
+ * vm_unmap_aliases - unmap outstanding lazy aliases in the vmap layer
+ *
+ * The vmap/vmalloc layer lazily flushes kernel virtual mappings primarily
+ * to amortize TLB flushing overheads. What this means is that any page you
+ * have now, may, in a former life, have been mapped into kernel virtual
+ * address by the vmap layer and so there might be some CPUs with TLB entries
+ * still referencing that page (additional to the regular 1:1 kernel mapping).
+ *
+ * vm_unmap_aliases flushes all such lazy mappings. After it returns, we can
+ * be sure that none of the pages we have control over will have any aliases
+ * from the vmap layer.
+ */
+void vm_unmap_aliases(void)
+{
+	unsigned long start = ULONG_MAX, end = 0;
+	int flush = 0;
+=======
+{
+	int cpu;
+>>>>>>> bbf3cb22f9 (treewide: Squash backport bpf from 5.10)
+
+	_vm_unmap_aliases(start, end, flush);
 }
 
 /**
@@ -2801,6 +2827,26 @@ void *vzalloc_node(unsigned long size, int node)
 			 GFP_KERNEL | __GFP_ZERO);
 }
 EXPORT_SYMBOL(vzalloc_node);
+
+/**
+ * vmalloc_user_node_flags - allocate memory for userspace on a specific node
+ * @size: allocation size
+ * @node: numa node
+ * @flags: flags for the page level allocator
+ *
+ * The resulting memory area is zeroed so it can be mapped to userspace
+ * without leaking data.
+ *
+ * Return: pointer to the allocated memory or %NULL on error
+ */
+void *vmalloc_user_node_flags(unsigned long size, int node, gfp_t flags)
+{
+	return __vmalloc_node_range(size, SHMLBA,  VMALLOC_START, VMALLOC_END,
+				    flags | __GFP_ZERO, PAGE_KERNEL,
+				    VM_USERMAP, node,
+				    __builtin_return_address(0));
+}
+EXPORT_SYMBOL(vmalloc_user_node_flags);
 
 /**
  * vmalloc_exec - allocate virtually contiguous, executable memory
