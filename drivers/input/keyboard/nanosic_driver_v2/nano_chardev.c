DD3Boh/android_kernel_xiaomi_sm8250

drivers: keyboard: nanosic_v2: Use hall sensor value
 * While userspace does provide info based on some calculations
   from the accelerometer, if hall sensor is triggered, it
   should have the priority.

Change-Id: Ib96bb0c71f3540da7c4ba502de709fae5b932a93

drivers/input/keyboard/nanosic_driver_v2/nano_chardev.c
@@ -427,7 +427,7 @@ static ssize_t Nanosic_chardev_fops_write(struct file *file,
 		if(Nanosonic_get_device_registered())
 			ret = Nanosic_input_release();
 	} else if (data[0] == 0x32 && data[1] == 0xFF && data[2] == 0x01) {
-		if(!Nanosonic_get_device_registered()){
+		if (!Nanosonic_get_device_registered() && gpio_get_value(gpio_hall_n_pin)) {
 			Nanosic_set_caps_led(0);
 			ret = Nanosic_input_register();
 		}

KNXDroid/xiaomi_sm8250_kernel

Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

drivers/input/keyboard/nanosic_driver_v2/nano_chardev.c
@@ -427,7 +427,7 @@ static ssize_t Nanosic_chardev_fops_write(struct file *file,
 		if(Nanosonic_get_device_registered())
 			ret = Nanosic_input_release();
 	} else if (data[0] == 0x32 && data[1] == 0xFF && data[2] == 0x01) {
-		if(!Nanosonic_get_device_registered()){
+		if (!Nanosonic_get_device_registered() && gpio_get_value(gpio_hall_s_pin)) {
 			Nanosic_set_caps_led(0);
 			ret = Nanosic_input_register();
 		}