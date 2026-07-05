17a9f26e2fecc24c4e4c45ffa1e591b47b40d086
danya2271 <danya2271@yandex.ru>
2026-07-05 20:16:46 +0500

    sched/core: remove affinity fallback printk

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2432,18 +2432,6 @@ static int select_fallback_rq(int cpu, struct task_struct *p)
 	}
 
 out:
-	if (state != cpuset) {
-		/*
-		 * Don't tell them about moving exiting tasks or
-		 * kernel threads (both mm NULL), since they never
-		 * leave kernel.
-		 */
-		if (p->mm && printk_ratelimit()) {
-			printk_deferred("process %d (%s) no longer affine to cpu%d\n",
-					task_pid_nr(p), p->comm, cpu);
-		}
-	}
-
 	return dest_cpu;
 }
 
