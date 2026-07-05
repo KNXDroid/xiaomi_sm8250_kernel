9275764e6f017f6f83790e592043a6f690e5e3a4
danya2271 <danya2271@yandex.ru>
2026-07-05 20:41:33 +0500

    mhi: reduce normal-build logging overhead

--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -952,27 +952,28 @@ char *mhi_get_restart_reason(const char *name);
 
 #else
 
-#define MHI_VERB(fmt, ...) do { \
-		if (mhi_cntrl->log_buf && \
-		    (mhi_cntrl->log_lvl <= MHI_MSG_LVL_VERBOSE)) \
-			ipc_log_string(mhi_cntrl->log_buf, "[D][%s] " fmt, \
-				       __func__, ##__VA_ARGS__); \
-} while (0)
+#define MHI_VERB(fmt, ...)
 
 #endif
 
 #define MHI_CNTRL_LOG(fmt, ...) do { \
 		if (mhi_cntrl->klog_lvl <= MHI_MSG_LVL_INFO) \
 			pr_err("[I][%s] " fmt, __func__, ##__VA_ARGS__);\
-		ipc_log_string(mhi_cntrl->cntrl_log_buf, "[I][%s] " fmt, \
-			       __func__, ##__VA_ARGS__); \
+		if (mhi_cntrl->cntrl_log_buf && \
+		    (mhi_cntrl->log_lvl <= MHI_MSG_LVL_INFO)) \
+			ipc_log_string(mhi_cntrl->cntrl_log_buf, \
+				       "[I][%s] " fmt, __func__, \
+				       ##__VA_ARGS__); \
 } while (0)
 
 #define MHI_CNTRL_ERR(fmt, ...) do { \
 		if (mhi_cntrl->klog_lvl <= MHI_MSG_LVL_ERROR) \
 			pr_err("[E][%s] " fmt, __func__, ##__VA_ARGS__); \
-		ipc_log_string(mhi_cntrl->cntrl_log_buf, "[E][%s] " fmt, \
-			       __func__, ##__VA_ARGS__); \
+		if (mhi_cntrl->cntrl_log_buf && \
+		    (mhi_cntrl->log_lvl <= MHI_MSG_LVL_ERROR)) \
+			ipc_log_string(mhi_cntrl->cntrl_log_buf, \
+				       "[E][%s] " fmt, __func__, \
+				       ##__VA_ARGS__); \
 } while (0)
 
 #define MHI_LOG(fmt, ...) do {	\
