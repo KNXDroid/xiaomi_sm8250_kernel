ebef1a1b3b0a5b0997fd94855b68bb0ac08ac8cd
danya2271 <danya2271@yandex.ru>
2026-07-06 12:37:08 +0500

    optimize smb5 register dump

--- drivers/power/supply/qcom/smb5-lib-munch.c
+++ drivers/power/supply/qcom/smb5-lib-munch.c
@@ -3668,35 +3668,50 @@ int smblib_night_charging_func(struct smb_charger *chg,
 }
 
 #define PERIPHERAL_MASK		0xFF
-static u16 peripheral_base;
-static char log[256] = "";
-static char version[8] = "smb:01:";
+struct smb_reg_dump {
+	u16 peripheral_base;
+	size_t len;
+	char log[256];
+};
+
+static inline void dump_reg_flush(struct smb_reg_dump *dump)
+{
+	if (!dump->len)
+		return;
+
+	dump->len += scnprintf(dump->log + dump->len,
+			sizeof(dump->log) - dump->len, "\n");
+	printk("%s", dump->log);
+	dump->len = 0;
+}
+
 static inline void dump_reg(struct smb_charger *chg, u16 addr,
-		const char *name)
+		const char *name, struct smb_reg_dump *dump)
 {
 	u8 reg = 0;
 	int rc;
-	char reg_data[50] = "";
+	u16 peripheral_base;
 
 	if (NULL == name) {
-		strlcat(log, "\n", sizeof(log));
-		printk(log);
+		dump_reg_flush(dump);
 		return;
 	}
 
 	rc = smblib_read(chg, addr, &reg);
 	if (rc < 0)
 		smblib_err(chg, "Couldn't read OTG status rc=%d\n", rc);
+
 	/* print one peripheral base registers in one line */
-	if (peripheral_base != (addr & ~PERIPHERAL_MASK)) {
-		peripheral_base = addr & ~PERIPHERAL_MASK;
-		memset(log, 0, sizeof(log));
-		snprintf(reg_data, sizeof(reg_data), "%s%04x ", version, peripheral_base);
-		strlcat(log, reg_data, sizeof(log));
+	peripheral_base = addr & ~PERIPHERAL_MASK;
+	if (dump->peripheral_base != peripheral_base) {
+		dump_reg_flush(dump);
+		dump->peripheral_base = peripheral_base;
+		dump->len = scnprintf(dump->log, sizeof(dump->log),
+				"smb:01:%04x ", peripheral_base);
 	}
-	memset(reg_data, 0, sizeof(reg_data));
-	snprintf(reg_data, sizeof(reg_data), "%02x ", reg);
-	strlcat(log, reg_data, sizeof(log));
+	if (dump->len < sizeof(dump->log))
+		dump->len += scnprintf(dump->log + dump->len,
+				sizeof(dump->log) - dump->len, "%02x ", reg);
 
 	smblib_dbg(chg, PR_REGISTER, "%s - %04X = %02X\n",
 							name, addr, reg);
@@ -3704,79 +3719,80 @@ static inline void dump_reg(struct smb_charger *chg, u16 addr,
 
 static void dump_regs(struct smb_charger *chg)
 {
+	struct smb_reg_dump dump = { .peripheral_base = 0xffff };
 	u16 addr;
 
 	/* charger peripheral */
 	for (addr = 0x6; addr <= 0xE; addr++)
-		dump_reg(chg, CHGR_BASE + addr, "CHGR Status");
+		dump_reg(chg, CHGR_BASE + addr, "CHGR Status", &dump);
 
 	for (addr = 0x10; addr <= 0x1B; addr++)
-		dump_reg(chg, CHGR_BASE + addr, "CHGR INT");
+		dump_reg(chg, CHGR_BASE + addr, "CHGR INT", &dump);
 
 	for (addr = 0x50; addr <= 0x70; addr++)
-		dump_reg(chg, CHGR_BASE + addr, "CHGR Config");
+		dump_reg(chg, CHGR_BASE + addr, "CHGR Config", &dump);
 
-	dump_reg(chg, CHGR_BASE + addr, NULL);
+	dump_reg(chg, CHGR_BASE + addr, NULL, &dump);
 
 	for (addr = 0x10; addr <= 0x1B; addr++)
-		dump_reg(chg, BATIF_BASE + addr, "BATIF INT");
+		dump_reg(chg, BATIF_BASE + addr, "BATIF INT", &dump);
 
 	for (addr = 0x50; addr <= 0x52; addr++)
-		dump_reg(chg, BATIF_BASE + addr, "BATIF Config");
+		dump_reg(chg, BATIF_BASE + addr, "BATIF Config", &dump);
 
 	for (addr = 0x60; addr <= 0x62; addr++)
-		dump_reg(chg, BATIF_BASE + addr, "BATIF Config");
+		dump_reg(chg, BATIF_BASE + addr, "BATIF Config", &dump);
 
 	for (addr = 0x70; addr <= 0x71; addr++)
-		dump_reg(chg, BATIF_BASE + addr, "BATIF Config");
+		dump_reg(chg, BATIF_BASE + addr, "BATIF Config", &dump);
 
-	dump_reg(chg, BATIF_BASE + addr, NULL);
+	dump_reg(chg, BATIF_BASE + addr, NULL, &dump);
 
 	for (addr = 0x6; addr <= 0x10; addr++)
-		dump_reg(chg, USBIN_BASE + addr, "USBIN Status");
+		dump_reg(chg, USBIN_BASE + addr, "USBIN Status", &dump);
 
 	for (addr = 0x12; addr <= 0x19; addr++)
-		dump_reg(chg, USBIN_BASE + addr, "USBIN INT ");
+		dump_reg(chg, USBIN_BASE + addr, "USBIN INT ", &dump);
 
 	for (addr = 0x40; addr <= 0x43; addr++)
-		dump_reg(chg, USBIN_BASE + addr, "USBIN Cmd ");
+		dump_reg(chg, USBIN_BASE + addr, "USBIN Cmd ", &dump);
 
 	for (addr = 0x58; addr <= 0x70; addr++)
-		dump_reg(chg, USBIN_BASE + addr, "USBIN Config ");
+		dump_reg(chg, USBIN_BASE + addr, "USBIN Config ", &dump);
 
 	for (addr = 0x80; addr <= 0x84; addr++)
-		dump_reg(chg, USBIN_BASE + addr, "USBIN Config ");
+		dump_reg(chg, USBIN_BASE + addr, "USBIN Config ", &dump);
 
-	dump_reg(chg, USBIN_BASE + addr, NULL);
+	dump_reg(chg, USBIN_BASE + addr, NULL, &dump);
 
 	for (addr = 0x06; addr <= 0x1B; addr++)
-		dump_reg(chg, TYPEC_BASE + addr, "TYPEC Status");
+		dump_reg(chg, TYPEC_BASE + addr, "TYPEC Status", &dump);
 
 	for (addr = 0x42; addr <= 0x72; addr++)
-		dump_reg(chg, TYPEC_BASE + addr, "TYPEC Config");
+		dump_reg(chg, TYPEC_BASE + addr, "TYPEC Config", &dump);
 
-	dump_reg(chg, TYPEC_BASE + 0x44, "TYPEC MODE CFG");
-	dump_reg(chg, TYPEC_BASE + addr, NULL);
+	dump_reg(chg, TYPEC_BASE + 0x44, "TYPEC MODE CFG", &dump);
+	dump_reg(chg, TYPEC_BASE + addr, NULL, &dump);
 
 	for (addr = 0x6; addr <= 0x10; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC Status");
+		dump_reg(chg, MISC_BASE + addr, "MISC Status", &dump);
 
 	for (addr = 0x15; addr <= 0x1B; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC INT");
+		dump_reg(chg, MISC_BASE + addr, "MISC INT", &dump);
 
 	for (addr = 0x51; addr <= 0x62; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC Config");
+		dump_reg(chg, MISC_BASE + addr, "MISC Config", &dump);
 
 	for (addr = 0x70; addr <= 0x76; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC Config");
+		dump_reg(chg, MISC_BASE + addr, "MISC Config", &dump);
 
 	for (addr = 0x80; addr <= 0x84; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC Config");
+		dump_reg(chg, MISC_BASE + addr, "MISC Config", &dump);
 
 	for (addr = 0x90; addr <= 0x94; addr++)
-		dump_reg(chg, MISC_BASE + addr, "MISC Config");
+		dump_reg(chg, MISC_BASE + addr, "MISC Config", &dump);
 
-	dump_reg(chg, MISC_BASE + addr, NULL);
+	dump_reg(chg, MISC_BASE + addr, NULL, &dump);
 }
 
 #define CHARGING_PERIOD_S		600
