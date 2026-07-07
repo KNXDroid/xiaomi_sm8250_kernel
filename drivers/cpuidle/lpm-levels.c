8efdf06cb7c517a8b02691ccc368f43a64d093d9
danya2271 <danya2271@yandex.ru>
2026-07-07 15:38:11 +0500

    cpuidle: lpm-levels: expose DT residency values

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -649,7 +649,7 @@ static int cluster_cpuidle_register(struct lpm_cluster *cl)
 
 			st->flags = 0;
 			st->exit_latency = cpu_level->pwr.exit_latency;
-			st->target_residency = 0;
+			st->target_residency = cpu_level->pwr.min_residency;
 			st->enter = lpm_cpuidle_enter;
 			if (i == lpm_cpu->nlevels - 1)
 				st->enter_s2idle = lpm_cpuidle_s2idle;
